40d09c3ccec29a1668f3ae4cda2bfd027d64d061
YuS2001 <s505507917@gmail.com>
2024-06-21 17:23:39 -0700

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import './css/body.css';
-import './css/app.css';
-import './css/header.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './app.css';
+import './body.css';
+import './header.css';
 
-import App from './App.jsx';
-
+ReactDOM.render(<App />, document.getElementById('app'));
