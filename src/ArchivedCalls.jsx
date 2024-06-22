7326df3780701f0d049192b43f85b93e17d629b4
YuS2001 <s505507917@gmail.com>
2024-06-21 17:37:22 -0700

    ArchivedCalls.jsx

--- src/ArchivedCalls.jsx
+++ src/ArchivedCalls.jsx
@@ -4,9 +4,12 @@ const ArchivedCalls = ({ calls, onUnarchive }) => (
   <div>
     <h2>Archived Calls</h2>
     <ul>
-      {calls.filter(call => call.isArchived).map(call => (
-        <li key={call.id}>
-          {call.detail}
+      {calls.filter(call => call.is_archived).map(call => (
+        <li key={call.id} className="archived-item">
+          <div>
+            <div>{call.from} tried to call {call.to}</div>
+            <div>{new Date(call.created_at).toLocaleString()}</div>
+          </div>
           <button onClick={() => onUnarchive(call.id)}>Unarchive</button>
         </li>
       ))}
